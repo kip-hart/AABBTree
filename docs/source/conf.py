6cc97e595b1d9572389afb164716b4be0d955f13
Kenneth Hart <khart@gatech.edu>
2019-01-18 19:39:16 -0500

    updated to version 1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Georgia Tech Research Corporation'
 author = 'Kenneth Hart'
 
 # The short X.Y version
-version = '0.0'
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.0-alpha'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
