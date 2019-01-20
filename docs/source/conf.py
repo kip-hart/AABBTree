ef57f06ec4b0f2a319aa865f7b0eeb4531783b6e
Hart, Kenneth Arthur <khart@gatech.edu>
2019-01-20 01:23:31 -0500

    updated versions to 1.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Kenneth Hart'
 # The short X.Y version
 version = '1.2'
 # The full version, including alpha/beta/rc tags
-release = '1.2'
+release = '1.2.1'
 
 
 # -- General configuration ---------------------------------------------------
