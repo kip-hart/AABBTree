030c63bdbc20858281c2079555b2a398b03d1ae5
Hart, Kenneth Arthur <khart@gatech.edu>
2019-01-20 14:18:11 -0500

    added option to remove blank pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -180,6 +180,9 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
+
+    # Remove extra blank pages
+    'extraclassoptions': 'openany,oneside'
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
