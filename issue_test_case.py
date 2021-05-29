5a1e85f4ba13675efd0963c80f3a6bd6f855ccab
Kip Hart <kiphart91@gmail.com>
2021-05-28 23:07:49 -0400

    updated test case

--- issue_test_case.py
+++ issue_test_case.py
@@ -5,5 +5,13 @@ aabb1 = AABB([(0, 0)])
 aabb2 = AABB([(-1, 0)])
 tree.add(aabb1, 'box 1')
 tree.add(aabb2, 'box 2')
+# Open Boxes
 v = tree.overlap_values(aabb2)
 print(v)
+# Closed Boxes
+v = tree.overlap_values(aabb2, closed=True)
+print(v)
+
+
+print(aabb1.overlaps(aabb2, closed=False))
+print(aabb1.overlaps(aabb2, closed=True))
