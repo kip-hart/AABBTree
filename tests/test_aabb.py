b07ee9e9667923ee651aca8d2111a013b721e6e7
Kip Hart <41959581+kip-hart@users.noreply.github.com>
2021-05-28 23:56:45 -0400

    add option to specify open/closed overlap testing (#14)

--- tests/test_aabb.py
+++ tests/test_aabb.py
@@ -108,6 +108,21 @@ def test_overlaps():
     assert not aabb2.overlaps(aabb3)
 
 
+def test_overlaps_closed():
+    aabb1 = AABB([(0, 0)])
+    aabb2 = AABB([(-1, 0)])
+    aabb3 = AABB([(1, 2)])
+    aabb4 = AABB([(-9, -8)])
+
+    assert aabb1.overlaps(aabb2, True)
+    assert aabb2.overlaps(aabb1, True)
+    assert not aabb1.overlaps(aabb3, True)
+    assert not aabb2.overlaps(aabb3, True)
+    assert not aabb1.overlaps(aabb4, True)
+    assert not aabb2.overlaps(aabb4, True)
+    assert not aabb1.overlaps(AABB(), True)
+
+
 def test_corners():
     lims = [(0, 10), (5, 10)]
     aabb_corners = [
